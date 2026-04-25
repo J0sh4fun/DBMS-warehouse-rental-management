dabd01105d9bf21a90f7eb014575f67f78af8863
tiendat8438 <tiendat9320@gmail.com>
2026-04-25 16:15:00 +0700

    Initialize project

---
+++ backend/src/main/java/com/example/dbmswarehouserentalmanagement/DbmsWarehouseRentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbmswarehouserentalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsWarehouseRentalManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbmsWarehouseRentalManagementApplication.class, args);
+    }
+
+}
